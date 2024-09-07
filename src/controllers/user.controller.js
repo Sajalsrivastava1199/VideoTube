6de25d65a47b3edfa9a6554441f74a279780f128
Devanshchowdhury2212 <devanshchowdhury@gmail.com>
2024-09-07 21:04:51 +0530

    Added Middleware logout Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,21 @@ import { user } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/fileupload.js";
 import { apiResponse } from "../utils/apiResponse.js";
 
+const generateAccessAndRefreshToken = async (userId) =>{
+    try {
+        const User = await user.findById(userId)
+        const refreshToken = User.generateRefreshToken()
+        const accessToken = User.generateAccessToken()
+        user.refreshToken = refreshToken
+        user.accessToken = accessToken
+        await user.save({validateBeforeSave:false})
+        return {refreshToken,accessToken}
+        
+    } catch (error) {
+        throw new apiError(500,"Error while AT and RT Generation")
+        
+    }
+}
 
 
 const registerUser = asyncHandler( async (req,res) => {
@@ -35,7 +50,7 @@ const registerUser = asyncHandler( async (req,res) => {
     if (existingUser){
         throw new apiError(409,"Username or Email already exists")
     }
-    
+
     const avatarLocalPath = req.files?.avatar?.[0]?.path
     // const coverImageLocalPath = req.files?.coverImage[0]?.path
 
@@ -77,5 +92,78 @@ const registerUser = asyncHandler( async (req,res) => {
     )
 })
 
-export {registerUser} 
+const loginUser = asyncHandler(async (req,res) => {
+    // req body -- > username,password
+    // check if username exist --> validate password
+    // AT RT give to user
+    // send in cookies
+
+    const {email,username,password} = req.body
+    if(!email && !username){
+        throw new apiError(400,"Username or Email is required")
+    }
+    
+    const User = await user.findOne({
+        $or:[{username},{email}]
+    })
+
+    if(User){
+        throw new apiError(404,"User Does Not Exists")
+    }
+
+    const isPasswordValid = await User.isPasswordCorrect(password)
+    // this method is avaialble from the object returned to us by mongoose model
+    if(!isPasswordValid){
+        throw new apiError(401,"Invalid Credentials")
+    }
+
+    // AT RT
+    const {refreshToken,accessToken} = await generateAccessAndRefreshToken(User._id)
+    const loggedinuser = await User.findById(User._id).
+    select("-password -refreshToken")
+
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new apiResponse(
+            200,
+            {
+                user:loggedinuser,accessToken,refreshToken
+            },
+            "User Logged in Successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async(req,res) =>{
+        const Userid = req?._id
+        await user.findByIdAndUpdate(
+            Userid,
+            {
+                $set:{"refreshToken":undefined}
+            },
+            {  
+                new:true
+            }
+        )
+        const options = {
+            httpOnly:true,
+            secure:true
+        }
+        return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options)
+        .json(new apiResponse(200,{},"User logged Out"))
+
+    }
+)
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser  
+} 
 
---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+import { apiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { user } from "../models/user.models.js";
+
+
+const verifyJWT = asyncHandler(async(req,_,next,error) =>{
+    try {
+        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        if(!accessToken){
+            throw new apiError(401,"Unauthorized request")
+        }
+        const decodedAccessToken = await jwt.verify(accessToken,process.env.ACCESS_TOKEN_SECRET)
+        const User = await user.findById(decodedAccessToken?._id).
+        select("-password -refreshToken")
+    
+        if(!User){
+            throw new apiError(401,"Invalid Access Token")
+        }
+        req.user = User
+        next()
+    } catch (error) {
+        throw new apiError(401,error?.message||"Invalid Access TOKEN")
+    }
+    
+
+})
+
+export {verifyJWT}
