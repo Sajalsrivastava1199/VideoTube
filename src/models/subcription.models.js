0b838a5b4739893bf3fb5bffd936e4f6252e530a
Devanshchowdhury2212 <devanshchowdhury@gmail.com>
2024-09-08 15:51:25 +0530

    Subscriptions Model Defined

---
+++ src/models/subcription.models.js
@@ -0,0 +1,18 @@
+import mongoose,{Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,// one who is subscriping
+        ref:"user"
+    },
+    channel:{
+        type:Schema.Types.ObjectId,// The owner of channel
+        ref:"user"
+    },
+
+
+},{
+    timestamps:true
+})
+
+export const subscription = model("subscription",subscriptionSchema) 
